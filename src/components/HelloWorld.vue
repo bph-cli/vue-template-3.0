66762bf13fb61eaa251a3afbf8048849cc37b94d
bphweb <m13296592591@163.com>
2022-04-19 17:53:21 +0800

    feat(dd): dd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,7 @@
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/HelloWorld.vue</code> test hot module replacement.
   </p>
 </template>
 
